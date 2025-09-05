25a3704ca4e8c2fa49e3fd2dc01e7c8fe6a8946b
McLo1 <marcelo.conceicao1718@gmail.com>
2025-09-05 00:15:23 -0300

    Inserido código de delete no server.js

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,6 @@ let todos = [];
 
 app.get('/api/todos', (req,res)=> res.json(todos));
 app.post('/api/todos', (req,res)=> { todos.push(req.body); res.json({msg:'Adicionado'}); });
+app.delete('/api/todos/:id', (req, res) => { const { id } = req.params;  res.json({ msg: 'Removido' });});
+
 app.listen(3000, ()=> console.log('Backend rodando na porta 3000'));
